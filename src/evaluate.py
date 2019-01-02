8172dcc03034a1b9a9f5631133f735f26680ae22
CoffeddCat <CoffeeddCat@github.com>
2019-01-02 12:57:24 +0000

    update.

---
+++ src/evaluate.py
@@ -0,0 +1,42 @@
+from config import *
+from model import Network
+import numpy as np
+import wdbedding
+try:
+    import xml.etree.cElementTree as ET
+except ImportError:
+    import xml.etree.ElementTree as ET
+
+if __name__ == '__main__':
+
+    network = Network("training")
+
+    word_vec_model = wdbedding.load_word2vec_model(EN)
+    print("word2vec model_loaded.")
+
+    input_file_path = '../data/task2_input_en.xml'
+    xmltree = ET.parse(input_file_path)
+    xmlroot = xmltree.getroot()
+
+    for review in xmlroot:
+        txt = review.text
+        if txt[-1] == '\n':
+            txt = txt[:-1]
+
+        print(txt)
+
+        test_data = []
+        test_data.append(wdbedding.embedding(word_vec_model, txt, EN))
+
+        test_data = np.reshape(test_data, (-1, Max_sentence_length, Embedding_dim, 1))
+        result = network.get_result(test_data)
+
+        print(result)
+
+        if (result[0][0]>0.5):
+            review.set("polarity","1")
+        else:
+            review.set("polarity", "-1")
+
+    output_file_path = '../data/task2_output_en.xml'
+    xmltree.write(output_file_path, encoding="utf-8")
